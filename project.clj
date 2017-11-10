eea813df2efc4730b1618ddf8e1fdb1ff0b8c15d
Egg Syntax <egg@novonon.com>
2017-11-10 12:43:40 -0500

    lein-release plugin: preparing 0.1.12 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datawalk "0.1.12-SNAPSHOT"
+(defproject datawalk "0.1.12"
   :description "A single-purpose tool for rapid REPL exploration of complex data structures"
   :url "https://github.com/eggsyntax/datawalk"
   :license {:name "Eclipse Public License"
