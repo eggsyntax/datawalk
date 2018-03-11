00e503808f2476363141fcac0cfb5f274ff189ad
Egg Syntax <egg@novonon.com>
2018-03-11 17:04:23 -0400

    This is gonna be version 2.0!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datawalk "0.1.14-SNAPSHOT"
+(defproject datawalk "2.0.0-SNAPSHOT"
   :description "A single-purpose tool for rapid REPL exploration of complex data structures"
   :url "https://github.com/eggsyntax/datawalk"
   :license {:name "Eclipse Public License"
